2a82c1b5e5d655f56e4c82765eaae316d689248c
Exzap <13877693+Exzap@users.noreply.github.com>
2025-12-02 14:42:49 +0100

    refactor/rpl: Treat HLE libs more like RPL modules
    HLE modules now have a unified interface via which they can get notified when mapped into the process or when loaded/unloaded.
    
    We also always call the unload functions when stopping emulation to give the module implementations a chance to reset all state (although many of them are still missing proper cleanup code for now)

--- src/Cafe/CafeSystem.cpp
+++ src/Cafe/CafeSystem.cpp
@@ -389,8 +389,9 @@ void cemu_initForGame()
 	ppcCyclesSince2000 = theTime * (uint64)ESPRESSO_CORE_CLOCK;
 	ppcCyclesSince2000TimerClock = ppcCyclesSince2000 / 20ULL;
 	PPCTimer_start();
-	// this must happen after the RPX/RPL files are mapped to memory (coreinit sets up heaps so that they don't overwrite RPX/RPL data)
-	osLib_load();
+	// coreinit is bootstrapped first and then the main game executable is loaded
+	RPLLoader_LoadCoreinit();
+	LoadMainExecutable();
 	// link all modules
 	uint32 linkTimeStart = GetTickCount();
 	RPLLoader_UpdateDependencies();
@@ -627,21 +628,6 @@ namespace CafeSystem
 		iosu::iosuAcp_init();
 		iosu::nim::Initialize();
 		iosu::odm::Initialize();
-		// init Cafe OS
-		avm::Initialize();
-		drmapp::Initialize();
-		TCL::Initialize();
-		nn::cmpt::Initialize();
-		nn::ccr::Initialize();
-		nn::temp::Initialize();
-		nn::aoc::Initialize();
-		nn::pdm::Initialize();
-		snd::user::Initialize();
-		H264::Initialize();
-		snd_core::Initialize();
-		mic::Initialize();
-		nfc::Initialize();
-		ntag::Initialize();
 		// init hardware register interfaces
 		HW_SI::Initialize();
 	}
@@ -772,7 +758,7 @@ namespace CafeSystem
         }
     }
 
-	PREPARE_STATUS_CODE SetupExecutable()
+	PREPARE_STATUS_CODE PrepareExecutable()
 	{
 		// set rpx path from cos.xml if available
 		_pathToBaseExecutable = _pathToExecutable;
@@ -803,7 +789,6 @@ namespace CafeSystem
 				}
 			}
 		}
-		LoadMainExecutable();
 		return PREPARE_STATUS_CODE::SUCCESS;
 	}
 
@@ -836,7 +821,7 @@ namespace CafeSystem
 		// setup memory space and PPC recompiler
         SetupMemorySpace();
         PPCRecompiler_init();
-		r = SetupExecutable(); // load RPX
+		r = PrepareExecutable(); // load RPX
 		if (r != PREPARE_STATUS_CODE::SUCCESS)
 			return r;
 		InitVirtualMlcStorage();
@@ -881,7 +866,7 @@ namespace CafeSystem
         SetupMemorySpace();
         PPCRecompiler_init();
         // load executable
-        SetupExecutable();
+        PrepareExecutable();
 		InitVirtualMlcStorage();
 		return PREPARE_STATUS_CODE::SUCCESS;
 	}
@@ -1067,7 +1052,7 @@ namespace CafeSystem
         GX2::_GX2DriverReset();
         nn::save::ResetToDefaultState();
         coreinit::__OSDeleteAllActivePPCThreads();
-        RPLLoader_ResetState();
+        RPLLoader_UnloadAll();
 		for(auto it = s_iosuModules.rbegin(); it != s_iosuModules.rend(); ++it)
 			(*it)->TitleStop();
         // reset Cemu subsystems
--- src/gui/wxgui/MainWindow.cpp
+++ src/gui/wxgui/MainWindow.cpp
@@ -2164,9 +2164,9 @@ void MainWindow::RecreateMenu()
 	}
 	else
 	{
-		// add 'Stop emulation' menu entry to file menu
 #ifdef CEMU_DEBUG_ASSERT
-		m_fileMenu->Append(MAINFRAME_MENU_ID_FILE_END_EMULATION, _("Stop emulation"));
+		m_fileMenu->Append(MAINFRAME_MENU_ID_FILE_END_EMULATION, _("Close game"));
+		m_fileMenuSeparator1 = m_fileMenu->AppendSeparator();
 #endif
 	}
 
